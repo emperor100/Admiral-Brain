48c1ac5fc589b3600eb62288bb72fa27bbe27f97
Ravikant <ravikantpandey88@gmail.com>
2023-11-25 13:26:03 +0530

    Add javascript concepts

---
+++ tech/languages/js/javascript.js
@@ -0,0 +1,15 @@
+/* Hoisting */
+
+// Functions and variables are stored in memory for an execution context before we execute our code. This is called hoisting.
+// Functions are stored with a reference to the entire functions, variables with the var keyword with the value of undefined, and variables with the let and const keyword are stored uninitialized.
+
+console.log(sum(2, 5)); //Since function is stored as reference, it will print 7
+console.log(number); // This will throw reference error
+console.log(name); // This will be undefined at this point.
+
+function sum(a, b) {
+  return a+b;
+}
+
+const number = 10;
+var name = 'Ravi';
